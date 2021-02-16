25c3171c1918f301215297b8859a640894c26e55
albinagossou <agossou@DESKTOP-89GMBAE.localdomain>
2021-02-16 15:53:52 +0100

    api call users

---
+++ front/services/usersServices.js
@@ -0,0 +1,36 @@
+let apiClient
+
+export function setClient(
+  globalApiClient,
+  onRequest = null,
+  onResponse = null,
+  onError = null
+) {
+  apiClient = globalApiClient.create()
+  apiClient.setBaseURL(apiClient.defaults.baseURL + '/users')
+  if (onRequest) clientOnRequest(onRequest)
+  if (onResponse) clientOnResponse(onResponse)
+  if (onError) clientOnError(onError)
+}
+
+export function clientOnRequest(onRequest) {
+  if (!apiClient) throw new Error('Api client not installed.')
+  apiClient.onRequest(onRequest)
+}
+
+export function clientOnResponse(onResponse) {
+  if (!apiClient) throw new Error('Api client not installed.')
+  apiClient.onResponse(onResponse)
+}
+
+export function clientOnError(onError) {
+  if (!apiClient) throw new Error('Api client not installed.')
+  apiClient.onError(onError)
+}
+
+export default {
+  getMyUsers() {
+    if (!apiClient) throw new Error('Api client not installed.')
+    return apiClient.get()
+  },
+}
