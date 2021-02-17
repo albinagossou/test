e601cd0a1f36f1c64da86b61ed07d0fef6627e41
albinagossou <agossou@DESKTOP-89GMBAE.localdomain>
2021-02-17 17:13:40 +0100

    modif dans api/companies.py

--- back/heqtor/web/api/companies.py
+++ back/heqtor/web/api/companies.py
@@ -4,9 +4,9 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 
 
 
-from heqtor.controllers import (get_all_company)
+from heqtor.controllers import (get_all_companies)
 
 class Companies(Resource):
   @jwt_required
   def get(self):
-    return get_all_company()
+    return get_all_companies() 
