1de735d3477284bc509c3ef598f827a80d2e4dbb
albinagossou <agossou@DESKTOP-89GMBAE.localdomain>
2021-02-18 17:03:16 +0100

    update on component Usercard

--- front/components/UserCard.vue
+++ front/components/UserCard.vue
@@ -5,38 +5,38 @@
       <v-list-item-group>
         <v-list-item>
           <v-list-item-icon>
-            <v-icon>mdi-email</v-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="user.email"></v-list-item-title>
-            </v-list-item-content>
+            <v-icon>mdi-email </v-icon>
           </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="user.email"></v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-icon>
-            <v-icon>mdi-account-circle-outline</v-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="user.firstName"></v-list-item-title>
-            </v-list-item-content>
+            <v-icon>mdi-account-circle-outline </v-icon>
           </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="user.firstName"></v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-icon>
-            <v-icon>mdi-account-circle</v-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="user.lastName"></v-list-item-title>
-            </v-list-item-content>
+            <v-icon>mdi-account-circle </v-icon>
           </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="user.lastName"></v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-icon>
-            <v-icon>mdi-phone</v-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="user.phone"></v-list-item-title>
-            </v-list-item-content>
+            <v-icon>mdi-phone </v-icon>
           </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="user.phone"></v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
     </v-list>
