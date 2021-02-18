f91dbe23bbdf013dc169b353d7634846fa15d6d7
albinagossou <agossou@DESKTOP-89GMBAE.localdomain>
2021-02-18 13:38:33 +0100

    component Usercard with props

---
+++ front/components/UserCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card>
+    <v-list>
+      <v-subheader>Carte Utilisateur</v-subheader>
+      <v-list-item-group>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-email</v-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="user.email"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item-icon>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-account-circle-outline</v-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="user.firstName"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item-icon>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-account-circle</v-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="user.lastName"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item-icon>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-phone</v-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="user.phone"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+// { firstName: 'Albin', lastName: 'Agossou', email: 'albin.agossou@heqtor.fr', phone: '0033123456789' }
+// { firstName: 'Albin', lastName: 'Agossou', email: 'albin.agossou@heqtor.fr' }
+</script>
