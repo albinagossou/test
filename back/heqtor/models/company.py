c326ad1d92bef10afb43bb5180be0b4f6a00e672
albinagossou <agossou@DESKTOP-89GMBAE.localdomain>
2021-02-22 13:36:59 +0100

    get_small_data users_qty

--- back/heqtor/models/company.py
+++ back/heqtor/models/company.py
@@ -1,19 +1,19 @@
 from sqlalchemy import Column, String
 
 from .base import Base, IdPkMixin
-from .user import User
+
 
 class Company(Base, IdPkMixin):
-    def get_small_data(self):
-        b = get_small_data()
-        if self.company is not None:
-            b['users_qty'] = self.company.users 
+    def get_small_data(User):
+        b = super().get_small_data()
+        if User.company is not None:
+            b['users_qty'] = User.company.users
         return b
-
     name = Column(String(35), nullable=False)
     siren = Column(String(9), unique=True, nullable=False)
     email = Column(String(50), nullable=False)
     phone = Column(String(13), nullable=False)
+
  
 #return admin name
 def admin_name():
