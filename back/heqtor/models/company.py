0c810c4afbcb27e7de4672d73dc6210ce6a32d73
albinagossou <agossou@DESKTOP-89GMBAE.localdomain>
2021-02-22 12:51:16 +0100

    users_qty: quantité d'utilisateurs via get_small_data

--- back/heqtor/models/company.py
+++ back/heqtor/models/company.py
@@ -1,9 +1,15 @@
 from sqlalchemy import Column, String
 
 from .base import Base, IdPkMixin
-
+from .user import User
 
 class Company(Base, IdPkMixin):
+    def get_small_data(self):
+        b = get_small_data()
+        if self.company is not None:
+            b['users_qty'] = self.company.users 
+        return b
+
     name = Column(String(35), nullable=False)
     siren = Column(String(9), unique=True, nullable=False)
     email = Column(String(50), nullable=False)
