14d174e5cb0b59946e74e54cfe3b29bb5aa6798e
albinagossou <agossou@DESKTOP-89GMBAE.localdomain>
2021-02-25 13:12:40 +0100

    init des routes API

--- back/heqtor/web/api/__init__.py
+++ back/heqtor/web/api/__init__.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 from heqtor.core import db, Session
 from heqtor.web import app
 
-from .me import Me, MeCompany, MeCompanyUsers
+from .me import Me, MeCompany, MeCompanyUsers, MeCompanyUser
 from .companies import Companies
 from .users import Users
 
@@ -15,8 +15,8 @@ api.add_resource(Me, "/me")
 api.add_resource(MeCompany, "/me/company")
 api.add_resource(Users, "/users")
 api.add_resource(Companies, "/companies")
-api.add_resource(MeCompany, "/me/company/users")
-api.add_resource(MeCompany, "/me/company/users/<int:user_id")
+api.add_resource(MeCompanyUsers, "/me/company/users")
+api.add_resource(MeCompanyUser, "/me/company/<int:user_id>")
 
 
 
